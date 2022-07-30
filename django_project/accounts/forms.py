5d8f9dec1ec7a143aeb58acbf796a864006fce03
Roqeeb <77676155+roqeebYusuff@users.noreply.github.com>
2022-07-30 08:24:47 +0100

    Blog API setup

---
+++ django_project/accounts/forms.py
@@ -0,0 +1,13 @@
+from dataclasses import fields
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('name',)
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
